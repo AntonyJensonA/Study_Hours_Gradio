08709c3eff17872b32c1653c2f4fe3ef0b2aef18
AntonyJensonA <143073662+AntonyJensonA@users.noreply.github.com>
2026-09-18 12:48:04 +0530

    Update app_gradio.py

--- app_gradio.py
+++ app_gradio.py
@@ -1,10 +1,16 @@
+import gradio as gr
+import joblib
+import pandas as pd
+import os
+
 model = joblib.load("Student_Std_Hrs.pkl")
 
 
-def predict_result(study_hours):
+def predict_result(study_hours, attendance):
 
     input_data = pd.DataFrame({
-        "Study_Hours": [study_hours]
+        "StudyHours": [study_hours],
+        "Attendance": [attendance]
     })
 
     prediction = model.predict(input_data)[0]
@@ -22,15 +28,26 @@ def predict_result(study_hours):
 
 demo = gr.Interface(
     fn=predict_result,
-    inputs=gr.Number(
-        label="Enter Study Hours",
-        minimum=0,
-        maximum=24,
-        value=5
-    ),
+
+    inputs=[
+        gr.Number(
+            label="Enter Study Hours",
+            minimum=0,
+            maximum=24,
+            value=5
+        ),
+        gr.Number(
+            label="Enter Attendance",
+            minimum=0,
+            maximum=100,
+            value=75
+        )
+    ],
+
     outputs=gr.Textbox(label="Prediction"),
+
     title="Student Result Prediction",
-    description="Predict Pass or Fail based on Study Hours."
+    description="Predict Pass or Fail based on Study Hours and Attendance."
 )
 
 
